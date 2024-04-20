b0e774c227190ab34e007d51fda16866f3cb7966
Kareena Patel <kareenapat17@gmail.com>
2024-04-20 09:51:24 +0100

    Changed title of project from simple... to digital...

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,9 @@
 import React from "react"
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faLinkedin } from "@fortawesome/free-brands-svg-icons";
 
 export default function Header() {
     return (
         <header>
-            <h2 className="title">Simple Profile Card Maker</h2>
+            <h2 className="title">Digital Profile Card Maker</h2>
         </header>
     )
 }
